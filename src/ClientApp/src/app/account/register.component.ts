0c070bc7828031233e62c2c4c91efc55036fa619
Matthew Tremper <Matthew.Tremper@ipreo.com>
2017-11-06 15:53:21 -0500

    Create login and registration pages

---
+++ src/ClientApp/src/app/account/register.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { NgRedux } from '@angular-redux/store';
+import { VoidwellApi } from '../shared/services/voidwell-api.service';
+import { IAppState } from '../app.component';
+import { Observable } from 'rxjs/Observable';
+import { NgForm } from '@angular/forms';
+
+@Component({
+    selector: 'voidwell-register',
+    templateUrl: './register.template.html',
+    styleUrls: ['../app.styles.css'],
+    providers: [VoidwellApi]
+})
+
+export class RegisterComponent implements OnDestroy {
+    self = this;
+    errorMessage: string = null;
+
+    constructor() {
+
+    }
+
+    onSubmit(registerForm: NgForm) {
+        console.log('submit', registerForm.value);
+    }
+
+    ngOnDestroy() {
+
+    }
+}
