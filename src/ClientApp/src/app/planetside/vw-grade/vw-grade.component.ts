6f55c29509f8b5b6753bec231d46130271d4fd98
Matt <matthew.tremper@ipreo.com>
2018-03-21 22:06:13 -0400

    Add grade calculations

---
+++ src/ClientApp/src/app/planetside/vw-grade/vw-grade.component.ts
@@ -0,0 +1,54 @@
+﻿import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { PerformanceGrades } from './../performance-grades.service';
+
+@Component({
+    selector: 'vw-grade',
+    template: '<span class="vw-grade" [ngClass]="gradeClass" [attr.title]="getDelta()">{{grade}}</span>',
+    styleUrls: ['./vw-grade.styles.css']
+})
+
+export class VWGradeComponent implements OnInit, OnDestroy {
+    @Input() delta: number;
+
+    grade: string = '???';
+    gradeClass: string;
+
+    private gradeSub: Subscription;
+
+    constructor(private gradesService: PerformanceGrades) {        
+    }
+
+    ngOnInit() {
+        this.gradeSub = this.gradesService.Grades.subscribe(grades => {
+            if (grades == null) { return; }
+
+            let newGrade;
+            for (let i = 0; i < grades.length; i++) {
+                if (this.delta > grades[i].delta) {
+                    newGrade = grades[i].grade;
+                    break;
+                }
+            }
+
+            if (!newGrade) {
+                newGrade = grades[grades.length - 1].grade;
+            }
+
+            this.grade = newGrade;
+            this.gradeClass = 'grade-' + this.grade[0];
+        });
+    }
+
+    public getDelta() {
+        var sDelta = "Δ";
+        if (this.delta) {
+            sDelta += Number(this.delta).toPrecision(3);
+        }
+        return sDelta;
+    }
+
+    ngOnDestroy() {
+        this.gradeSub.unsubscribe();
+    }
+}
