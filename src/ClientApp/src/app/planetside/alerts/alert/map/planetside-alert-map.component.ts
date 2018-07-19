e35d8ebe13034eb1f6ea4d35e12229cae41cd846
Matt <matthew.tremper@ipreo.com>
2018-07-19 15:40:27 -0400

    Better replay map seeking. Alert map time line filtering. Alert list paging and filtering.

--- src/ClientApp/src/app/planetside/alerts/alert/map/planetside-alert-map.component.ts
+++ src/ClientApp/src/app/planetside/alerts/alert/map/planetside-alert-map.component.ts
@@ -1,6 +1,6 @@
 ﻿import { Component, OnInit, Injector, EventEmitter } from '@angular/core';
 import { DataSource } from '@angular/cdk/collections';
-import { Observable, of, BehaviorSubject } from 'rxjs';
+import { Observable, of, BehaviorSubject, merge } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -11,8 +11,14 @@ import { map } from 'rxjs/operators';
 export class PlanetsideAlertMapComponent implements OnInit {
     ownershipSub: BehaviorSubject<any> = new BehaviorSubject(null);
     focusFacilityEmitter: EventEmitter<any> = new EventEmitter<any>();
+    focusTimestampEmitter: EventEmitter<any> = new EventEmitter<any>();
     alert: any;
     dataSource: TableDataSource;
+
+    filterState = {
+        captures: true,
+        defends: false
+    };
 
     constructor(private injector: Injector) {
     }
@@ -29,11 +35,21 @@ export class PlanetsideAlertMapComponent implements OnInit {
             this.alert = alert;
             this.ownershipSub.next(alert.zoneSnapshot);
             this.dataSource = new TableDataSource(alert.log.captureLog);
+            this.dataSource.filter = this.filterState;
         });
     }
 
     onFocusFacility(mapRegionId) {
         this.focusFacilityEmitter.emit(mapRegionId);
+    }
+
+    onFocusTimestamp(timestamp) {
+        this.focusTimestampEmitter.emit(timestamp);
+    }
+
+    onFilterToggle(event) {
+        this.filterState[event.value] = event.source.checked;
+        this.dataSource.filter = this.filterState;
     }
 }
 
@@ -42,11 +58,24 @@ export class TableDataSource extends DataSource<any> {
         super();
     }
 
+    _filterChange = new BehaviorSubject(null);
+    get filter() { return this._filterChange.value; }
+    set filter(filter) { this._filterChange.next(filter); }
+
     connect(): Observable<any[]> {
         let first = of(this.data);
-        return first.pipe(map(() => {
+        return merge(first, this._filterChange).pipe(map(() => {
             const data = this.data.slice();
-            return data;
+
+            if (!this.filter) {
+                return data;
+            }
+
+            let filteredData = data.filter(item => {
+                return (this.filter.captures && item.newFactionId !== item.oldFactionId) || (this.filter.defends && item.newFactionId === item.oldFactionId)
+            });
+
+            return filteredData;
         }));
     }
 
