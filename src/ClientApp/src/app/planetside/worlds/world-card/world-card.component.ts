69a5401a67caed812a83610878b651399e5b495f
Jess Tremper <jess@voidwell.com>
2018-08-31 14:32:57 -0400

    Redesign worlds page

---
+++ src/ClientApp/src/app/planetside/worlds/world-card/world-card.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'world-card',
+    templateUrl: './world-card.template.html',
+    styleUrls: ['./world-card.styles.css']
+})
+
+export class WorldCardComponent {
+    @Input() world: any;
+}
