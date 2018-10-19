87947cad55d2305b5b590d175a2a426278b2f229
Jess Tremper <jess@voidwell.com>
2018-10-19 14:12:00 -0400

    Cleanup packages

--- src/ClientApp/src/main.ts
+++ src/ClientApp/src/main.ts
@@ -1,8 +1,6 @@
-﻿import * as $ from 'jquery';
-window['$'] = window['jQuery'] = window['jquery'] = $;
+﻿import 'reflect-metadata';
+import 'zone.js';
 
-import 'reflect-metadata';
-import 'bootstrap';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
