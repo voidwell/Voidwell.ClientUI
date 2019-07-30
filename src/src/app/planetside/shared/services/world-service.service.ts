33965d079b0e6b704614adbbbb5e3a7c6d520a80
Lampjaw <matthewt89@gmail.com>
2019-07-29 21:08:55 -0400

    Add Playstation 4 support

--- src/src/app/planetside/shared/services/world-service.service.ts
+++ src/src/app/planetside/shared/services/world-service.service.ts
@@ -1,16 +1,38 @@
-﻿import { BehaviorSubject } from 'rxjs';
-import { Injectable } from '@angular/core';
+﻿import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
 import { PlanetsideApi } from './planetside-api.service';
+import reducers from './../../planetside.reducers';
+import { WithSubStore, select } from '@angular-redux/store';
+
+@WithSubStore({
+    basePathMethodName: 'getBasePath',
+    localReducer: reducers
+})
 
 @Injectable()
-export class WorldService {
+export class WorldService implements OnDestroy {
+    @select('platform') readonly platform$: Observable<any>;
+    private platformSub: Subscription;
+    private worldSub: Subscription;
+
     public Worlds: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
     constructor(private ps2Api: PlanetsideApi) {
-        ps2Api.getAllWorlds().subscribe(worlds => {
-            if (worlds != null) {
-                this.Worlds.next(worlds);
-            }
+        this.platformSub = this.platform$.subscribe(platformState => {
+            this.worldSub = this.ps2Api.getAllWorlds().subscribe(worlds => {
+                if (worlds != null) {
+                    this.Worlds.next(worlds);
+                }
+            });
         });
+    }
+
+    ngOnDestroy(): void {
+        if (this.platformSub) this.platformSub.unsubscribe();
+        if (this.worldSub) this.worldSub.unsubscribe();
+    }
+
+    private getBasePath() {
+        return ['ps2'];
     }
 }
