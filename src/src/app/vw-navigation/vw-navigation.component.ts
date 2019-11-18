0c4c46567a18f06ebe0e695fd9164337775c448a
Lampjaw <matthewt89@gmail.com>
2019-11-18 12:31:53 -0500

    Remove Planetside Arena module

--- src/src/app/vw-navigation/vw-navigation.component.ts
+++ src/src/app/vw-navigation/vw-navigation.component.ts
@@ -1,5 +1,5 @@
 ﻿import { Component, ChangeDetectorRef, OnDestroy, NgZone } from '@angular/core';
-import { RouterLinkActive, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { NestedTreeControl } from '@angular/cdk/tree';
@@ -99,13 +99,6 @@ const NAV_DATA: NavNode[] = [
                 children: []
             }
         ]
-    },
-    {
-        name: 'Planetside Arena',
-        url: '/psa',
-        icon: 'mdi-gamepad-variant',
-        exact: false,
-        children: []
     }
 ];
 
