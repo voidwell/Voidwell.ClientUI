db3c7abc2456d8a91d639da5c4d76fb4171a78f1
Lampjaw <matthewt89@gmail.com>
2020-04-11 22:58:01 -0400

    Implement model mapping

--- src/src/app/admin/oidc/clients/client-details/client-details.component.ts
+++ src/src/app/admin/oidc/clients/client-details/client-details.component.ts
@@ -1,15 +1,20 @@
 ﻿import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Subscription, throwError } from 'rxjs';
 import { catchError, finalize } from 'rxjs/operators';
 import { VoidwellApi } from './../../../../shared/services/voidwell-api.service';
+
+const GRANT_TYPES = [
+    "delegation",
+    "client_credentials",
+    "implicit"
+];
 
 @Component({
     templateUrl: './client-details.template.html',
     styleUrls: ['./client-details.styles.css']
 })
-
 export class ClientDetailsComponent implements OnDestroy {
     isLoading: boolean;
     errorMessage: string;
@@ -21,21 +26,9 @@ export class ClientDetailsComponent implements OnDestroy {
     constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private api: VoidwellApi) {
         this.isLoading = true;
 
-        this.form = this.formBuilder.group({
-            id: new FormControl(),
-            name: ['', Validators.required],
-            description: new FormControl(),
-            startDate: new FormControl(),
-            endDate: new FormControl(),
-            isPrivate: new FormControl(false),
-            mapId: new FormControl(),
-            serverId: new FormControl(),
-            gameId: new FormControl('ps2'),
-            teams: this.formBuilder.array([])
-        });
-
         this.routeSub = this.route.params.subscribe(params => {
             let clientId = params['clientId'];
+            this.isLoading = true;
 
             this.api.getClientById(clientId)
                 .pipe<any>(catchError(error => {
@@ -47,6 +40,7 @@ export class ClientDetailsComponent implements OnDestroy {
                 }))
                 .subscribe(client => {
                     this.client = client;
+                    this.createForm(client);
                 });
         });
     }
@@ -55,6 +49,57 @@ export class ClientDetailsComponent implements OnDestroy {
         if (this.form.invalid) {
             return;
         }
+    }
+
+    createForm(config) {
+        let allowedGrantTypes = this.formBuilder.array(GRANT_TYPES.map(t => new FormControl({selected: config.allowedGrantTypes.indexOf(t) > -1, name: t})));
+
+        this.form = this.formBuilder.group({
+            enabled: new FormControl(config.enabled),
+            clientId: new FormControl(config.clientId),
+            clientName: new FormControl(config.clientName),
+            clientSecrets: new FormControl(config.clientSecrets),
+            requireClientSecret: new FormControl(config.requireClientSecret),
+            description: new FormControl(config.description),
+            allowedGrantTypes: allowedGrantTypes,
+            requirePkce: new FormControl(config.requirePkce),
+            allowPlainTextPkce: new FormControl(config.allowPlanTextPkce),
+            allowAccessTokensViaBrowser: new FormControl(config.allowAccessTokensViaBrowser),
+            redirectUris: this.formBuilder.array(config.redirectUris.map(t => new FormControl(t))),
+            postLogoutRedirectUris: this.formBuilder.array(config.postLogoutRedirectUris.map(t => new FormControl(t))),
+            frontChannelLogoutUri: new FormControl(config.frontChannelLogoutUri),
+            frontChannelLogoutSessionRequired: new FormControl(config.frontChannelLogoutSessionRequired),
+            backChannelLogoutUri: new FormControl(config.backChannelLogoutUri),
+            backChannelLogoutSessionRequired: new FormControl(config.backChannelLogoutSessionRequired),
+            allowOfflineAccess: new FormControl(config.allowOfflineAccess),
+            allowedScopes: this.formBuilder.array(config.allowedScopes.map(t => new FormControl(t))),
+            alwaysIncludeUserClaimsInIdToken: new FormControl(config.alwaysIncludeUserClaimsInIdToken),
+            identityTokenLifetime: new FormControl(config.identityTokenLifetime),
+            accessTokenLifetime: new FormControl(config.accessTokenLifetime),
+            authorizationCodeLifetime: new FormControl(config.authorizationCodeLifetime),
+            absoluteRefreshTokenLifetime: new FormControl(config.absoluteRefreshTokenLifetime),
+            slidingRefreshTokenLifetime: new FormControl(config.slidingRefreshTokenLifetime),
+            refreshTokenUsage: new FormControl(config.refreshTokenUsage.toString()),
+            updateAccessTokenClaimsOnRefresh: new FormControl(config.updateAccessTokenClaimsOnRefresh),
+            refreshTokenExpiration: new FormControl(config.refreshTokenExpiration.toString()),
+            accessTokenType: new FormControl(config.accessTokenType.toString()),
+            enableLocalLogin: new FormControl(config.enableLocalLogin),
+            claims: this.formBuilder.array(config.claims.map(t => new FormControl(t))),
+            alwaysSendClientClaims: new FormControl(config.alwaysSendClientClaims),
+            clientClaimsPrefix: new FormControl(config.clientClaimsPrefix),
+            allowedCorsOrigins: this.formBuilder.array(config.allowedCorsOrigins.map(t => new FormControl(t))),
+            includeJwtId: new FormControl(config.includeJwtId),
+
+            scopeInput: new FormControl(),
+            redirectUriInput: new FormControl(),
+            postLogoutRedirectUriInput: new FormControl(),
+            allowedCorsOriginsInput: new FormControl(),
+            claimsInput: new FormControl()
+        });
+    }
+
+    removeFromControlList(control) {
+        control.parent.removeAt(control.parent.controls.indexOf(control));
     }
 
     ngOnDestroy() {
